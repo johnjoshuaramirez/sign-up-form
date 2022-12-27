4146ec026e054bbacfa7043336159ed7cbe7aacc
John Joshua Ramirez <johnjoshuaramirez.social@gmail.com>
2022-12-26 17:13:44 -0800

    style: add background overlay design

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const input = document.getElementById("first-name");
-input.focus();
-input.select();
+const firstName = document.querySelector("#first-name");
+const leftContent = document.querySelector(".left-content");
+const rightContent = document.querySelector(".right-content");
 
-function create_circles(number) {
+function createOverlayOne(number) {
 	let dot_container = document.createElement("div");
 
 	for (let i = 0; i < 3; i++) {
@@ -20,9 +20,35 @@ function create_circles(number) {
 	}
 
 	dot_container.classList.add(`dot-container-${number}`);
-	document.body.appendChild(dot_container);
+	leftContent.appendChild(dot_container);
 }
 
-create_circles("one");
-create_circles("two");
-create_circles("three")
+function createOverlayTwo(number, element) {
+	let dot_container = document.createElement("div");
+
+	for (let i = 0; i < 6; i++) {
+		let dot_group = document.createElement("div");
+
+		for (let j = 0; j < 3; j++) {
+			let dot = document.createElement("div");
+
+			dot.classList.add("dot");
+			dot_group.appendChild(dot);
+		}
+
+		dot_group.classList.add("dot-group");
+		dot_container.appendChild(dot_group);
+	}
+
+	dot_container.classList.add(`dot-container-${number}`);
+	element.appendChild(dot_container);
+}
+
+window.addEventListener("load", () => {
+	firstName.focus();
+	firstName.select();
+	createOverlayOne("one");
+	createOverlayOne("two");
+   createOverlayTwo("three", leftContent);
+   createOverlayTwo("four", rightContent);
+});
